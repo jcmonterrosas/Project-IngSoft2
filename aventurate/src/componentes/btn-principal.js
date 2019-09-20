a074b1d808dce23c2339525ccf74fd24a19c2c89
JhonatanSV <jsandovalv@unal.edu.co>
2019-09-20 00:06:48 -0500

    video agregado, botones terminados y acomodados

--- aventurate/src/componentes/btn-principal.js
+++ aventurate/src/componentes/btn-principal.js
@@ -1,73 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Btnprincipal extends Component{
-    constructor () {
-        super();
+export default class Btnprincipal extends Component {
+  constructor() {
+    super();
 
-        this.state={
-          hide1:true,
-          hide2:true,
-          card1:false,
-          card2:false
-        }
+    this.state = {
+      hide: true,
+      card: false
+    };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleOutputChange = this.handleOutputChange.bind(this);
-      }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleOutputChange = this.handleOutputChange.bind(this);
+  }
 
-      handleInputChange(e,a) {
-        const stylea={border:'1px solid rgba(0,0,0,.125'};
-        if(a==1){
-        this.setState({hide1:false,card1:true});
-        }
-        else{
-        this.setState({hide2:false,card2:true});
-        }
-      
-        
-      }
-
-      handleOutputChange(e) {
-        
-   
-        this.setState({hide1:true,hide2:true,card1:false,card2:false});
-        
-      }
-
-      render() {
-
-        const style1=this.state.hide1 ? {display:'none'} : {};
-        const style2=this.state.hide2 ? {display:'none'} : {};
-        const style3=this.state.card1 ? {borderRadius:'20px',border:'1px solid rgba(0,0,0,.125)',background:'#FFCE33'} : {background:'#2B3E50'};
-        const style4=this.state.card2 ? {borderRadius:'20px',border:'1px solid rgba(0,0,0,.125)',background:'#FFCE33'} : {background:'#2B3E50'};
-
-        return (
-      
-            <div className="container">
-                <div className="row">
-                  <div className="card col tarjetaBoton "  style={style3}>
-                  <button type="button" className="botonInicial btn btn-warning btn-lg btn-block"
-                    onMouseOver={() => this.handleInputChange(this,1)}
-                    onMouseOut={this.handleOutputChange}>Hotel</button>       
-                    <div className="card-body" style={style1} >
-                    <p className="card-text textoBoton" >¿Ya sabes a donde quieres ir?</p>
-                  </div>
-                </div>
-                <div className="col-4">
-                </div>
-                <div className="card col tarjetaBoton "  style={style4} >
-                  <button type="button" className="botonInicial btn btn-warning btn-lg btn-block" 
-                  onMouseOver={() => this.handleInputChange(this,2)}
-                  onMouseOut={this.handleOutputChange}>Actividad</button>       
-                    <div className="card-body"  style={style2} >
-                    <p className="card-text textoBoton" >¿Quieres hacer algo, pero no sabes donde?</p>
-                  </div>
-                </div>
-            </div>
-          </div>
-
-        )
-
-    }
+  handleInputChange(e, a) {
     
+      this.setState({ hide: false,card:true});
+   
+  }
+
+  handleOutputChange(e) {
+    this.setState({ hide: true,  card: false });
+  }
+
+  render() {
+    const style1 = this.state.hide ? { display: "none" } : {};
+    const style3 = this.state.card
+      ? {
+          borderRadius: "20px",
+          border: "1px solid rgba(0,0,0,.125)",
+          background: "#FFCE33"
+        }
+      : { background: "rgba(43, 62, 80,0)", border: "none" };
+    
+    return (
+      <div className="container">
+      <div className="row">
+          <div className="card col tarjetaBoton1 " style={style3}>
+            <button
+              type="button"
+              className="botonInicial1 btn btn-warning btn-lg btn-block"
+              onMouseOver={this.handleInputChange}
+              onMouseOut={this.handleOutputChange}
+            >
+             {this.props.buttontext}
+            </button>
+            <div className="card-body" style={style1}>
+              <p className="card-text textoBoton">
+                {this.props.cardtext}
+              </p>
+            </div>
+          </div> 
+          </div> 
+          </div>
+    );
+  }
 }
