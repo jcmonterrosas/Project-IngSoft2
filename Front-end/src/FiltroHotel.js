894502d3056546faa8b082297162bfe965d6626e
crimaco197 <crmartinezco@unal.edu.co>
2019-09-23 23:20:00 -0500

    primer commit - prueba

---
+++ Front-end/src/FiltroHotel.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import './BuscarHotel.css';
+
+class App extends Component{
+    state = {
+        nombre: '',
+        email: ''
+    };
+
+    render(){
+        return (
+            <div>
+            
+            <form>
+
+            </form>
+            </div>
+        );
+    }
+}
+
+export default App;
