9d68d43d6c91a8a11c2dab2e43b5d44df465375b
crimaco197 <crmartinezco@unal.edu.co>
2019-09-29 22:22:37 -0500

    Filtro (Hoteles, Actividades, Actualización de logos)

--- Front-end/src/BuscarHotel.js
+++ Front-end/src/BuscarHotel.js
@@ -1,76 +1,80 @@
-import React, { Component } from 'react';
-/*import {
-   // DropdownButton,
-    Dropdown
-  } from "react-dropdown"; */
-
-//import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React, { Component } from "react";
+import "./BuscarHotel.css";
+import './componentes/Filtros.css';
+import Buscador from "./componentes/Buscador";
+import Resultado from "./componentes/Resultado";
+import Btncambio from "./componentes/btn-cambio";
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import './BuscarHotel.css';
-import Buscador from './componentes/Buscador';
-import HotelesActividades from './componentes/Buscar_Hotel_Actividad';
-import Resultado from './componentes/Resultado';
 import FiltroHotel from './componentes/FiltroHotel';
 import FiltroActividad from './componentes/FiltroActividad';
-import './componentes/Filtros.css';
-
+import axios from "axios";
 
 
 import filtro from "./img/filtro.png";
 import logo from "./img/Logo_PRINCIPAL.png"
 
 class App extends Component {
-
     state = {
-        termino: '',
+        termino: "",
+        resultados: [],
         imagenes: [],
-        pagina: ''
-    }
+        pagina: ""
+    };
 
     paginaAnterior = () => {
         let pagina = this.state.pagina;
-        if (pagina !== 1)
-            pagina--;
-        else
-            return null;
-        this.setState({
-            pagina
-        }, () => {
-            this.consultarApi();
-        })
-    }
+        if (pagina !== 1) pagina--;
+        else return null;
+        this.setState(
+            {
+                pagina
+            },
+            () => {
+                this.consultarApi();
+            }
+        );
+    };
 
     paginaSiguiente = () => {
-
         let pagina = this.state.pagina;
         pagina++;
-        this.setState({
-            pagina
-        }, () => {
-            this.consultarApi();
-        });
+        this.setState(
+            {
+                pagina
+            },
+            () => {
+                this.consultarApi();
+            }
+        );
+    };
 
-    }
+    datosBusqueda = termino => {
+        this.setState(
+            {
+                termino: termino,
+                pagina: 1
+            },
+            () => {
+                this.consultarApi();
+            }
+        );
+    };
 
-    datosBusqueda = (termino) => {
-        this.setState({
-            termino: termino,
-            pagina: 1
-        }, () => {
-            this.consultarApi();
-        })
-    }
-
-    consultarApi = () => {
-        const pagina = this.state.pagina;
+    consultarApi = async () => {
+        /*const pagina = this.state.pagina;
         const url = `https://pixabay.com/api/?key=13669805-f32c8cf3c97ec348e21458fac&q=
-    ${this.state.termino}&per_page=30&page=${pagina}`;
+        ${this.state.termino}&per_page=30&page=${pagina}`;*/
 
-        fetch(url)
-            .then(respuesta => respuesta.json())
-            .then(resultado => this.setState({ imagenes: resultado.hits }))
+        const res = await axios.get(
+            `https://api-aventurate.herokuapp.com/hotels/${this.state.termino}`
+        );
+        this.setState({ resultados: res.data });
+        console.log(this.state.resultados);
 
-    }
+        /*fetch(url)
+          .then(respuesta => respuesta.json())
+          .then(resultado => this.setState({ imagenes: resultado.hits }));*/
+    };
 
     function() {
         "#Filtros".click(function () {
@@ -78,24 +82,26 @@ class App extends Component {
             this.ToggleClass('active');
 
         });
-    }
-
+    };
 
     render() {
         return (
             <div className="Hotel container">
                 <div className="jumbotron">
-                    
-                    <img  src={logo} height="30%" width="30%" alt="Logo" />
+                    <img src={logo} height="30%" width="30%" alt="Logo" />
                     <Buscador datosBusqueda={this.datosBusqueda} />
                 </div>
-                <div className="row">
-
-                    <div className="col-1"></div>
-                    <div className="col-9" align="right">
-                        <HotelesActividades />
-                        {/*<button type="button" className="btn btn-warning">Hotel</button>
-                        <button type="button" className="btn btn-warning" align="right">Actividad</button>*/}
+                <div className="row ">
+                    <div className="col-10 align-content-center" role="group" aria-label="Basic example">
+                        <Btncambio
+                            buttontext="Hoteles"
+                            disabled="true"
+                            link="/SearchHotel"
+                        />
+                        <Btncambio
+                            buttontext="Actividades"
+                            link="/SearchActivity"
+                        />
                     </div>
 
                     <div className="col-2" align="right">
@@ -109,17 +115,18 @@ class App extends Component {
                             </DropdownMenu>
                         </UncontrolledDropdown>
                     </div>
-                </div>
-                <div className="row justify-content-center">
+                    
                     <Resultado
+                        resultados={this.state.resultados}
                         imagenes={this.state.imagenes}
                         paginaAnterior={this.paginaAnterior}
-                        paginaSiguiente={this.paginaSiguiente} />
+                        paginaSiguiente={this.paginaSiguiente}
+                    />
                 </div>
-
             </div>
         );
     }
 }
 
 export default App;
+
--- Front-end/src/componentes/Buscador.js
+++ Front-end/src/componentes/Buscador.js
@@ -1,43 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Buscador.css";
 
 export default class Buscador extends Component {
+  busquedaRef = React.createRef();
 
-    busquedaRef = React.createRef();
+  ObtenerDatos = e => {
+    e.preventDefault();
+    const termino = this.busquedaRef.current.value;
+    this.props.datosBusqueda(termino);
+  };
 
-    ObtenerDatos = (e) => {
-        e.preventDefault();
-        const termino = this.busquedaRef.current.value;
-        this.props.datosBusqueda(termino);
-    }
-
-    render() {
-        return (
-
-            <form onSubmit={this.ObtenerDatos}>
-                <div className="row">
-                    <div className="form-group col">
-                        <input ref={this.busquedaRef} type="text" className="form-control"
-                            placeholder="Busca tu Imagen. Ejemplo: Futbol">
-                        </input>
-                    </div>
-                    <div className="col">
-                        <input type="date" className="form-control" ></input>
-                    </div>
-                    <div className="col">
-                        <input type="date" className="form-control" ></input>
-                    </div>
-                    <div className="col">
-                        <input type="number" className="form-control" placeholder="# personas"></input>
-                    </div>
-                    <div className="form-group col">
-                        <input type="submit" className="btn btn-lg btn-warning btn-block"
-                            value="Buscar">
-                        </input>
-                    </div>
-                </div>
-            </form>
-
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.ObtenerDatos}>
+        <div className="row filaBuscador">
+          <div className="form-group col">
+            <input
+              ref={this.busquedaRef}
+              type="text"
+              className="form-control Buscador "
+              placeholder="Busca Hoteles en tu destino. Ejemplo: Santa Marta"
+            ></input>
+          </div>
+          <div className="col input-group date">
+            <input type="date" className="form-control"></input>
+          </div>
+          <div className="col">
+            <input type="date" className="form-control"></input>
+          </div>
+          <div className="col">
+            <input
+              type="number"
+              className="form-control"
+              placeholder="# personas"
+            ></input>
+          </div>
+          <div className="form-group col">
+            <input
+              type="submit"
+              className="btn btn-lg btn-warning btn-block"
+              value="Buscar"
+            ></input>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
-
