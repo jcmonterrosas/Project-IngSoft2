1f12191c554725be27408ea1653955b165d4aa2a
jcmonterrosas <38740763+jcmonterrosas@users.noreply.github.com>
2019-10-25 18:36:37 -0500

    :zap::lipstick: Feature idpr003 registro (#37)
    * :sparkles::lipstick: ultimos cambios
    
    se agrega boton restablecer, el campo identificacion, y algunas reformas de estilos
    
    * Update Register.js

--- Front-end/src/Register.js
+++ Front-end/src/Register.js
@@ -15,28 +15,28 @@ class Register extends Component {
     };
 
 
-    valueToState = ({  value,id }) => {
-        switch(id){
+    valueToState = ({ value, id }) => {
+        switch (id) {
             case "Nombre":
-                    this.setState({Nombre: value});
+                this.setState({ Nombre: value });
                 break;
             case "Telefono":
-                    this.setState({Telefono: value});
+                this.setState({ Telefono: value });
                 break;
             case "Correo":
-                    this.setState({Correo: value});
+                this.setState({ Correo: value });
                 break;
             case "Contraseña":
-                    this.setState({Contraseña: value});
+                this.setState({ Contraseña: value });
                 break;
             case "TipoDocumento":
-                    this.setState({TipoDocumento: value});
+                this.setState({ TipoDocumento: value });
                 break;
             case "Rol":
-                    this.setState({Rol: value});
+                this.setState({ Rol: value });
                 break;
             case "FechaNacimiento":
-                    this.setState({FechaNacimiento: value});
+                this.setState({ FechaNacimiento: value });
                 break;
         }
         console.log(this.state);
@@ -45,11 +45,11 @@ class Register extends Component {
     render() {
         return (
             <div className="Register">
-                <div className="row">
+                <form className="row">
                     <div className="jumbotron-fluid Registerbody" align="center">
                         <div className="row">
                             <div className="col-12" align="center">
-                                <h1>Bienvenido</h1>
+                                <h1>Crea tu cuenta</h1>
                             </div>
                         </div>
                         <div className="row campos">
@@ -62,17 +62,17 @@ class Register extends Component {
                             </div>
                             <div className="col-6" align="center">
                                 <label for="inp" className="inp">
-                                    <input type="text" id="Telefono" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
+                                    <input type="tel" id="Telefono" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
                                     <span className="label">Telefono</span>
                                     <span className="border"></span>
                                 </label>
                             </div>
-                            <div/>
+                            <div />
                         </div>
                         <div className="row campos">
                             <div className="col-6" align="center">
                                 <label for="inp" className="inp">
-                                    <input type="text" id="Correo" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
+                                    <input type="email" id="Correo" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
                                     <span className="label">Correo</span>
                                     <span className="border"></span>
                                 </label>
@@ -84,53 +84,64 @@ class Register extends Component {
                                     <span className="border"></span>
                                 </label>
                             </div>
-                            <div/>
+                            <div />
                         </div>
                         <div className="row campos">
                             <div className="input-group mb-3 col-6">
-                            <select className="custom-select inp" id="TipoDocumento" onChange={event => this.valueToState(event.target)}>
-                                <option selected>Tipo de documento...</option>
-                                <option value="C.C">C.C</option>
-                                <option value="T.I">T.I</option>
-                                <option value="C.E">C.E</option>
-                                <option value="P.S">P.S</option>
-                            </select>
+                                <label for="inp" className="inp">
+                                    <input type="number" id="Identificacion" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
+                                    <span className="label">Identificación</span>
+                                    <span className="border"></span>
+                                </label>
                             </div>
                             <div className="input-group mb-3 col-6">
-                            <select className="custom-select inp" id="Rol" onChange={event => this.valueToState(event.target)}>
-                                <option selected>Rol...</option>
-                                <option value="Cliente">Cliente</option>
-                                <option value="Proveedor">Proveedor</option>
-                            </select>
+                                <select className="custom-select inp" id="TipoDocumento" onChange={event => this.valueToState(event.target)}>
+                                    <option selected>Tipo</option>
+                                    <option value="C.C">C.C</option>
+                                    <option value="T.I">T.I</option>
+                                    <option value="C.E">C.E</option>
+                                    <option value="P.S">P.S</option>
+                                </select>
                             </div>
+
                         </div>
                         <div className="row campos">
-                            <div className="col-2"></div>
-                            <div className="col-8 align-self-center" align="center">
+                            <div className="input-group mb-3 col-6">
+                                <select className="custom-select inp" id="Rol" onChange={event => this.valueToState(event.target)}>
+                                    <option selected>Rol</option>
+                                    <option value="Cliente">Cliente</option>
+                                    <option value="Proveedor">Proveedor</option>
+                                </select>
+                            </div>
+                            <div className="input-group mb-3 col-6">
                                 <label for="inp" className="inp">
                                     <input type="date" id="FechaNacimiento" placeholder="&nbsp;" onChange={event => this.valueToState(event.target)} />
                                     <span className="label">Fecha de Nacimiento</span>
                                     <span className="border"></span>
                                 </label>
                             </div>
-                            <div/>
+                            <div />
                         </div>
-                        
-
-
-                        <br />
-                        <div className="row" align="center">
-                            <div className="col-2" />
-                            <div className="col-8" >
-                                <Btnprincipal
-                                    buttontext='Registrarse'
-                                    link="/"
-                                />
-                                <div className="col-2" />
+                        <div className="row">
+                            <div className="input-group mb-3 col-6">
+                                <button
+                                    type="reset"
+                                    className="btn btn-warning btn-lg btn-block"
+                                >
+                                    Restablecer
+                                </button>
+                            </div>
+                            <div className="input-group mb-3 col-6">
+                                <button
+                                    type="button"
+                                    className="btn btn-warning btn-lg btn-block"
+                                >
+                                    Registrarse
+                                </button>
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         );
     }
