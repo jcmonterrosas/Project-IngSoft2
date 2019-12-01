16c1f1faf0301c7277dc28667858e0d73c25d7dc
Andres Aldana <agaldanaw@unal.edu.co>
2019-12-01 10:14:37 -0500

    listos modelos reserva :fire:

--- Back-end/src/models/Reserva.js
+++ Back-end/src/models/Reserva.js
@@ -1,30 +1,13 @@
-/*const mongoose = require("mongoose");
+
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const ActSchema = require("./Activities");
 
-const ResSchema = new Schema({
-  service: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Activities"
-  },
-  hotel: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Hotel"
-  },
-  name: { type: String, required: true }
-  /*numero_reserva: { type: String, required: true }, // se agrupa por este numero, hay que generar el numero aleatorio, por cada actividad se agrega una reserva.
-  fecha_inicio_act: { type: String, required: true },
-  cedula_cliente: { type: String, required: true },
-  numero_personas: { type: String, required: true },
-  telefono_responsable: { type: String, required: true },
-  direccion_responsable: { type: String, required: true },
-  fecha_fin_act: { type: String, required: true },
-  origen: { type: String, required: true },
-  destino: { type: String, required: true },
-  adultos: { type: String, required: true },
-  ninos: { type: String, required: true },
-  estado: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-}); 
+const ReservaSchema = new Schema({
+  usr_id: { type: String, required: true },
+  name: { type: String, required: true },
+  price_total: { type: String, required: true },
+  price_exclude_iva: { type: String, required: true },
+  state_pay: { type: String, required: true }
+});
 
-module.exports = mongoose.model("Reserva", ResSchema); */
+module.exports = mongoose.model("Reserva", ReservaSchema);
