4b37f4d5fd201fbf898d521d38618ff70b32f4c1
Tom Erick <perezalvareztomerick32@gmail.com>
2019-12-17 18:02:35 -0500

    :boom::bug: Fixes reserva hotel

--- Front-end/src/componentes/Imagen.js
+++ Front-end/src/componentes/Imagen.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./Services.css";
+import { setInStorage, getFromStorage } from "../storage";
 
 const Imagen = props => {
   const {
+    _id,
     name,
     departamento,
     ciudad,
@@ -9,40 +12,86 @@ const Imagen = props => {
     price_per_person,
     acommodation,
     phone,
-    images
+    images,
+    hab_ind,
+    hab_dob,
+    hab_fam,
+    hab_mul
   } = props.imagen;
 
+  function handleClick(e) {
+    e.preventDefault();
+    setInStorage("hotel_id", _id);
+  }
+
+  let contacto, acomodacion;
+  let cantHabInd, cantHabDob, cantHabFam, cantHabMul;
+  if (phone) {
+    contacto = (
+      <div>
+        <h5>Contacto:</h5>
+        <label>{phone}</label>
+      </div>
+    );
+  } else {
+    contacto = null;
+  }
+  if (hab_ind) {
+    cantHabInd = <label>{"Individuales: " + hab_ind}</label>;
+  }
+  if (hab_dob) {
+    cantHabDob = <label>{"Dobles: " + hab_dob}</label>;
+  }
+  if (hab_fam) {
+    cantHabFam = <label>{"Familiares: " + hab_fam}</label>;
+  }
+  if (hab_mul) {
+    cantHabMul = <label>{"Multiples: " + hab_mul}</label>;
+  }
+  if (acommodation) {
+    acomodacion = (
+      <div>
+        <h5>Habitaciones disponibles:</h5>
+        {cantHabInd}
+        <br />
+        {cantHabDob}
+        <br />
+        {cantHabFam}
+        <br />
+        {cantHabMul}
+      </div>
+    );
+  }
+
   return (
-    <div className="container card containerHotel">
-      <div className="row align-items-center">
-        <div className="col align-self-center">
-          <div className="container ">
-            <p className="row titleHotel">
-              <bold>{name}</bold>
-            </p>
-            <p className="row">Departamento: {departamento}</p>
-            <p className="row">Ciudad: {ciudad}</p>
-            <p className="row">Direccion: {address}</p>
-            <p className="row">Acomodación Disponible: {acommodation}</p>
-            <p className="row">Contacto: {phone}</p>
+    <div className="card container">
+      <div className="contCard">
+        <div className="colItems">
+          <div>
+            <h5>Ubicación: </h5>
+            <label>
+              {departamento ? departamento + " - " + ciudad : ciudad}
+            </label>
+            <br />
+            <label>{address} </label>
+          </div>
+          {contacto}
+          {acomodacion}
+          <div>
+            <h5>Precio: </h5>
+            <label>{"$ " + price_per_person}</label>
           </div>
         </div>
-
-        <div className="col colPrices">
-          <div className="container">
-            <p className="row">
-              <img src={images} width="300px" height="150px"></img>{" "}
-            </p>
-            <p className="row">Precio por persona: ${price_per_person}</p>
-            <a
-              href="#"
-              target="_blank"
-              className="btn btn-lg btn-warning btn-block"
-            >
-              {" "}
-              Ver detalles
-            </a>
-          </div>
+        <div className="colPrices">
+          <h2>{name}</h2>
+          <img src={images}></img>
+          <a
+            target="_blank"
+            className="btn btn-lg btn-warning btn-block"
+            onClick={handleClick}
+          >
+            Agregar a la reserva
+          </a>
         </div>
       </div>
     </div>
